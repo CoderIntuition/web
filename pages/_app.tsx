c9b8693333784e48ed86d69a8dce46e0e5a2754a
Linda Yang <lindayang0928@hotmail.com>
2021-02-21 00:31:30 -0500

    Add breakpoint provider back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { AppProps } from "next/app";
 import "tailwindcss/dist/base.min.css";
 import "fomantic/dist/semantic.css";
+import { BreakpointProvider } from "react-socks";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <BreakpointProvider>
+      <Component {...pageProps} />
+    </BreakpointProvider>
+  );
 };
 
 export default App;
