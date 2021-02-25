791edd6a1657cb71b18301b45f3f3f104538d048
David Zhang <david.zhang2@shopify.com>
2021-02-24 17:45:22 -0800

    Remove socks and twin

--- babel-plugin-macros.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  twin: {
-    config: 'tailwind.config.js',
-    preset: 'styled-components',
-    dataTwProp: true,
-    debugPlugins: false,
-    debug: false,
-  },
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import { AppProps } from "next/app";
 import "../styles/tailwind.css";
 import "fomantic/dist/semantic.css";
-import { BreakpointProvider } from "react-socks";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <BreakpointProvider>
-      <Component {...pageProps} />
-    </BreakpointProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default App;
